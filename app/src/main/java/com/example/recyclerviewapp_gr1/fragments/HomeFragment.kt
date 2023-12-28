27384d3c93c1e355cd39c6e26e81ca3841b38673
Shqipdon Mehmeti <keds30671@KEDS.kv>
2023-12-28 20:08:13 +0100

    first commit

---
+++ app/src/main/java/com/example/recyclerviewapp_gr1/fragments/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.recyclerviewapp_gr1.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recyclerviewapp_gr1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewapp_gr1/helpers/Helpers.kt
@@ -0,0 +1,45 @@
+package com.example.recyclerviewapp_gr1.helpers
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.recyclerviewapp_gr1.api.ServiceApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Helpers {
+    fun provideRetrofitInstance(): ServiceApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .build()
+            .create(ServiceApi::class.java)
+    }
+
+    fun provideSharedPreferences(context : Context) : SharedPreferences {
+        return context.getSharedPreferences("shared_prefs", Context.MODE_PRIVATE)
+    }
+
+    fun saveStringToSharedPrefs(context: Context, key : String, value : String) {
+        provideSharedPreferences(context).edit().apply {
+            putString(key,value)
+            apply() // replacement of commit() for working async
+        }
+    }
+
+    fun getStringFromSharedPrefs(context: Context, key : String) : String? {
+        return provideSharedPreferences(context).getString(key,"")
+    }
+
+    fun saveIntToSharedPrefs(context: Context, key : String, value : Int) {
+        provideSharedPreferences(context).edit().apply {
+            putInt(key,value)
+            apply() // replacement of commit() for working async
+        }
+    }
+
+    fun getIntFromSharedPrefs(context: Context, key : String) : Int {
+        return provideSharedPreferences(context).getInt(key,0)
+
+    }
+
+}
