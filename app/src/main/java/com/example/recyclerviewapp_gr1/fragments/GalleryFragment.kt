071f57db1a8ac6eef6d7e5319cc3d107c4258c01
Shqipdon Mehmeti <keds30671@KEDS.kv>
2024-01-16 18:19:51 +0100

    one way data binding

--- app/src/main/java/com/example/recyclerviewapp_gr1/fragments/GalleryFragment.kt
+++ app/src/main/java/com/example/recyclerviewapp_gr1/fragments/GalleryFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recyclerviewapp_gr1.databinding.FragmentGalleryBinding
+import com.example.recyclerviewapp_gr1.models.Person
 
 class GalleryFragment : Fragment() {
 
@@ -18,6 +19,16 @@ class GalleryFragment : Fragment() {
     ): View {
         binding = FragmentGalleryBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val person = Person(
+            firstName = "Filan",
+            lastName = "Fisteku",
+            age = 99
+        )
+        binding.personObject = person
     }
 
 
---
+++ app/src/main/java/com/example/recyclerviewapp_gr1/models/Person.kt
@@ -0,0 +1,7 @@
+package com.example.recyclerviewapp_gr1.models
+
+data class Person(
+    val firstName : String?,
+    val lastName : String?,
+    val age : Int?
+)
